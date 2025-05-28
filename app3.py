caa2975610eebe4f7f6c8399143df0d6ee9947d4
Malheiros76 <l_souza.malheiros@hotmail.com>
2025-05-28 09:02:19 -0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -43,9 +43,15 @@ def set_background(png_file):
 set_background("Design_sem_nome-removebg-preview.png")
 
 # LOGO E TÍTULO
-st.image("BRASÃO.png", width=150)
-st.markdown("## **Registro de Ocorrências – Colégio Cívico-Militar do Paraná**")
-st.markdown("---")
+doc.add_picture("BRASÃO.png", width=doc.sections[0].page_width * 0.2)
+
+doc.add_heading("COLÉGIO CÍVICO-MILITAR DO PARANÁ", level=1)
+doc.add_heading("REGISTRO DE OCORRÊNCIA DISCIPLINAR", level=2)
+
+doc.add_paragraph(f"**Aluno:** {dados_aluno['nome_aluno']}", style="Normal")
+doc.add_paragraph(f"**CGM:** {dados_aluno['cgm']} | **Turma:** {dados_aluno['turma']} | **Ano:** {dados_aluno['ano']}", style="Normal")
+doc.add_paragraph("")  # espaço
+doc.add_paragraph("**FATOS REGISTRADOS:**", style="Normal")
 
 # BANCO DE DADOS
 conn = sqlite3.connect("ocorrencias.db", check_same_thread=False)
